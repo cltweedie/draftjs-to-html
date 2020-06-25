7d3a91ade69cecb68a5289431943c32c94b597e5
Chris Tweedie <cltweedie@gmail.com>
2020-06-25 10:17:28 +0100

    Revert "Revert "Changes for liquid""
    This reverts commit 38b66ac36b0020a5e51a2a4a8953139980c6a00d.

--- js/block.js
+++ js/block.js
@@ -520,16 +520,20 @@ export function getBlockMarkup(
       customEntityTransform,
     ));
   } else {
-    const blockTag = getBlockTag(block.type);
+    if (block.text.startsWith('%liquid')) {
+      const blockTag = 'liquid';
+    } else {
+      const blockTag = getBlockTag(block.type);
+    }
     if (blockTag) {
       blockHtml.push(`<${blockTag}`);
       const blockStyle = getBlockStyle(block.data);
       if (blockStyle) {
         blockHtml.push(` style="${blockStyle}"`);
       }
-      if (directional) {
-        blockHtml.push(' dir = "auto"');
-      }
+      // if (directional) {
+      //   blockHtml.push(' dir = "auto"');
+      // }
       blockHtml.push('>');
       blockHtml.push(getBlockInnerMarkup(block, entityMap, hashtagConfig, customEntityTransform));
       blockHtml.push(`</${blockTag}>`);
